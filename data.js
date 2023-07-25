94ea613c0afa23a5a6076e161d7bb32c4deee6d7
rainmax777 <88648153+rainmax777@users.noreply.github.com>
2023-07-26 01:01:32 +0300

    3

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0---05_camera001_07-25-2023",
-      "name": "Люба Гостинная Бар05_Camera001_07-25-2023",
+      "id": "0---05_perspective_07-26-2023_01",
+      "name": "Люба Гостинная Бар05_Perspective_07-26-2023_01",
       "levels": [
         {
           "tileSize": 256,
@@ -16,59 +16,19 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "yaw": -0.09399132743556038,
-        "pitch": -0.09527361957979785,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.3200050055223613,
-          "pitch": -0.0014902983785667345,
-          "rotation": 0,
-          "target": "1-untitled_perspective_07-24-2023_01"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 0.8661950811874526,
-          "pitch": -0.31482726727332633,
-          "title": "Это кухня<br>",
-          "text": "Армстронг"
-        }
-      ]
-    },
-    {
-      "id": "1-untitled_perspective_07-24-2023_01",
-      "name": "Untitled_Perspective_07-24-2023_01",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
         },
         {
           "tileSize": 512,
-          "size": 512
+          "size": 2048
         }
       ],
-      "faceSize": 512,
+      "faceSize": 1500,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.4598822970178933,
-          "pitch": -0.10649451618622408,
-          "rotation": 1.5707963267948966,
-          "target": "0---05_camera001_07-25-2023"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
