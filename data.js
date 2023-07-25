829d795c3b393dc07b5305c316e0f898fbae86bb
rainmax777 <88648153+rainmax777@users.noreply.github.com>
2023-07-26 00:24:49 +0300

    1

---
+++ data.js
@@ -0,0 +1,82 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---05_camera001_07-25-2023",
+      "name": "Люба Гостинная Бар05_Camera001_07-25-2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.09399132743556038,
+        "pitch": -0.09527361957979785,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3200050055223613,
+          "pitch": -0.0014902983785667345,
+          "rotation": 0,
+          "target": "1-untitled_perspective_07-24-2023_01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8661950811874526,
+          "pitch": -0.31482726727332633,
+          "title": "Это кухня<br>",
+          "text": "Армстронг"
+        }
+      ]
+    },
+    {
+      "id": "1-untitled_perspective_07-24-2023_01",
+      "name": "Untitled_Perspective_07-24-2023_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4598822970178933,
+          "pitch": -0.10649451618622408,
+          "rotation": 1.5707963267948966,
+          "target": "0---05_camera001_07-25-2023"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3D Тур",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
